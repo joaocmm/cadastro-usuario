5bb0e1126de4d434540eec90c228698c7b59bb93
joaoc <jcmmonteiro@yahoo.com.br>
2026-01-13 11:22:43 -0300

    Entidade model

---
+++ src/main/java/m2works/cadastro_usuario/usuarios/model/Usuario.java
@@ -0,0 +1,36 @@
+package m2works.cadastro_usuario.usuarios.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    private String nome;
+
+    @Email(message = "Email inválido")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+    
+}
